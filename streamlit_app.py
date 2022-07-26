12b600de38e0aed0cd2875ef3152cdcd525260b2
snowflakesdg <109339288+snowflakesdg@users.noreply.github.com>
2022-07-26 10:18:03 +0200

    Update streamlit_app.py
    request

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 import pandas as pd
+import requests
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
